c44ce06febc153e9bc04fee4c6338394a186e1e5
Flávio Henrique Bonfim <flaviohbonfim@live.com>
2026-05-16 12:26:44 -0300

    Add localhost:5173 to allowed origins in CORS middleware for development

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3001",
+        "http://localhost:5173",
         "https://quickstash.apti.dev",
     ],
     allow_credentials=True,
